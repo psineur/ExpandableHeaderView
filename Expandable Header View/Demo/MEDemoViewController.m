29d7ca17e518e755b95648e3ee32ea98d3d22680
Stepan Generalov <psi.pk.ru@gmail.com>
2017-07-27 22:23:54 -0600

    Add headerViewShrinkedContent

--- Expandable Header View/Demo/MEDemoViewController.m
+++ Expandable Header View/Demo/MEDemoViewController.m
@@ -10,9 +10,11 @@
 #import "MEExpandableHeaderView.h"
 #import <Foundation/Foundation.h>
 
-@interface MEDemoViewController ()<UITableViewDataSource, UITableViewDelegate>
+@interface MEDemoViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property(nonatomic, strong) MEExpandableHeaderView *headerView;
+@property(nonatomic, strong) UIView *headerViewClipToBoundsSubview;
+@property(nonatomic, strong) UIView *headerViewShrinkedContent;
 @property(nonatomic, strong) IBOutlet UITableView *tableView;
 
 @property(nonatomic, retain) NSArray *elementsList;
@@ -33,10 +35,40 @@
 
 #pragma mark - Setup
 
+static const CGFloat kShrinkSize = 60.0f;
+static const CGFloat kFullSize = 240.0f;
 - (void)setupHeaderView
 {
-    _headerView = [[MEExpandableHeaderView alloc] initWithFrame:CGRectMake(0, 0, self.tableView.bounds.size.width, 240)];
+    _headerView = [[MEExpandableHeaderView alloc] initWithFrame:CGRectMake(0, 0, self.tableView.bounds.size.width, kFullSize)];
     self.headerView.backgroundImage = [UIImage imageNamed:@"beach"];
+    [self.headerView addSubview:({
+        UIView *clipToBoundsSubview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.tableView.bounds.size.width, kFullSize)];
+        clipToBoundsSubview.backgroundColor = UIColor.clearColor;
+        self.headerViewShrinkedContent = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.tableView.bounds.size.width, kShrinkSize)];
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.tableView.bounds.size.width, kShrinkSize)];
+        label.text = @"PLZ Refactor ME!";
+        label.font = [UIFont systemFontOfSize:24.0f];
+        label.contentMode = UIViewContentModeCenter;
+        [self.headerViewShrinkedContent addSubview:label];
+        clipToBoundsSubview.clipsToBounds = YES;
+        [clipToBoundsSubview addSubview:self.headerViewShrinkedContent];
+        self.headerViewClipToBoundsSubview = clipToBoundsSubview;
+    })];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    self.headerViewClipToBoundsSubview.frame = self.headerView.bounds;
+
+    [self _updateShrinkViewPosition];
+}
+
+- (void)_updateShrinkViewPosition
+{
+    CGFloat yPosition = 2 * (self.headerView.frame.size.height - kShrinkSize);
+    self.headerViewClipToBoundsSubview.frame = self.headerView.bounds;
+    self.headerViewShrinkedContent.frame = CGRectMake(0, yPosition, self.headerViewShrinkedContent.superview.bounds.size.width, kShrinkSize);
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
@@ -78,13 +110,9 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     NSString *cellIdentifier = @"defaultCell";
-    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    
     NSString *rowText = [self.elementsList objectAtIndex:indexPath.row];
-    
     cell.textLabel.text = rowText;
-    
     return cell;
 }
 
@@ -104,11 +132,11 @@
         [self.tableView beginUpdates];
         CGRect headerViewRect = self.headerView.frame;
         headerViewRect.origin = CGPointZero;
-        headerViewRect.size.height = MAX(self.headerView.originalHeight - scrollView.contentOffset.y, 28.0f);
+        headerViewRect.size.height = MAX(self.headerView.originalHeight - scrollView.contentOffset.y, kShrinkSize);
         self.headerView.frame = headerViewRect;
         [self.tableView endUpdates];
     }
-    
+    [self _updateShrinkViewPosition];
 }
 
 @end
