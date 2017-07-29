52752ca24a07f0db262dbf95c882cea485e1c631
Stepan Generalov <psi.pk.ru@gmail.com>
2017-07-28 21:45:38 -0600

    Add label and avatar for both shrieked / full size state

--- Expandable Header View/Demo/MEDemoViewController.m
+++ Expandable Header View/Demo/MEDemoViewController.m
@@ -38,6 +38,9 @@
 @implementation MEDemoViewController
 {
     UIImageView *_avatar;
+    UILabel *_label;
+
+    UIImageView *_shrinkedAvatar;
     UILabel *_shrinkedLabel;
 }
 
@@ -75,13 +78,27 @@ static const CGFloat kFullSize = 180.0f;
     // shrinkedContentView has fixed height and becomes visible when headerView is collapsed
     [self.headerView.shrinkedContentView addSubview:({
         _shrinkedLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.tableView.bounds.size.width, kShrinkSize)];
-        _shrinkedLabel.text = @"PLZ Refactor ME!";
+        _shrinkedLabel.text = @"John Smith";
         _shrinkedLabel.font = [UIFont systemFontOfSize:24.0f];
         _shrinkedLabel.contentMode = UIViewContentModeCenter;
         _shrinkedLabel;
     })];
+    [self.headerView.shrinkedContentView addSubview:({
+        _shrinkedAvatar = [[UIImageView alloc] initWithFrame:CGRectZero];
+        _shrinkedAvatar.contentMode = UIViewContentModeScaleToFill;
+        _shrinkedAvatar.clipsToBounds = YES;
+        _shrinkedAvatar.image = [UIImage imageNamed:@"defaultAvatar"];
+        _shrinkedAvatar;
+    })];
 
     // fullsizeContentView has fixed height and is hidden when headerView is collapsed
+    [self.headerView.fullsizeContentView addSubview:({
+        _label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.tableView.bounds.size.width, kShrinkSize)];
+        _label.text = @"John Smith";
+        _label.font = [UIFont systemFontOfSize:32.0f];
+        _label.textAlignment = NSTextAlignmentCenter;
+        _label;
+    })];
     [self.headerView.fullsizeContentView addSubview:({
         _avatar = [[UIImageView alloc] initWithFrame:CGRectZero];
         _avatar.contentMode = UIViewContentModeScaleToFill;
@@ -95,13 +112,18 @@ static const CGFloat kFullSize = 180.0f;
     self.headerView.onLayout = ^(UIView *headerView, UIView *fullsizeContentView, UIView *shrinkedContentView) {
         __strong __typeof(self) strongSelf = weakSelf;
         UIImageView *avatar = strongSelf->_avatar;
-        static CGFloat kTopMargin = 30.0f;
+        static CGFloat kTopMargin = 16.0f;
         CGFloat kAvatarSize = 128.0f;
         CGFloat bW = fullsizeContentView.bounds.size.width;
         avatar.frame = CGRectMake(0.5f * (bW - kAvatarSize), kTopMargin, kAvatarSize, kAvatarSize);
         avatar.layer.cornerRadius = 0.5f * kAvatarSize;
         avatar.layer.borderColor = UIColor.whiteColor.CGColor;
         avatar.layer.borderWidth = 6.0f;
+        strongSelf->_label.frame = CGRectMake(0, CGRectGetMaxY(avatar.frame) + 4.0f, weakSelf.tableView.bounds.size.width, 32.0f);
+
+        CGRect shrinkAvatarFrame = CGRectMake(0, 0, kShrinkSize, kShrinkSize);
+        strongSelf->_shrinkedAvatar.frame = shrinkAvatarFrame;
+        strongSelf->_shrinkedLabel.frame = CGRectMake(CGRectGetMaxX(shrinkAvatarFrame), 0, weakSelf.tableView.bounds.size.width, kShrinkSize);
     };
 
     // Set frame after initializing subviews to update layout.
